592462e7045c031279a47715013e7ce59e34c9f1
Vip3Outs <karokab270@student.polsl.pl>
2019-11-24 01:48:37 +0100

    Obsluga przycisku nowy bilet i blokowanie biletu

--- src/com/bdproj/EmployeeWgt.java
+++ src/com/bdproj/EmployeeWgt.java
@@ -5,14 +5,16 @@ import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.sql.*;
 public class EmployeeWgt {
     private JPanel panelMain;
     private JButton btnLogout;
     private JTextField textField1;
     private JCheckBox nowyBiletCheckBox;
     private JTextField textField2;
-    private JComboBox comboBox1;
+    private JComboBox pozcennika;
     private JButton wydrukujBiletButton;
     private JButton doładujBiletButton;
     private JTextField textField3;
@@ -22,7 +24,7 @@ public class EmployeeWgt {
 
     public EmployeeWgt(MainView mainView) {
         this.mainView = mainView;
-
+        wydrukujBiletButton.setEnabled(false);
 
         btnLogout.addActionListener(new ActionListener() {
             @Override
@@ -30,10 +32,71 @@ public class EmployeeWgt {
                 mainView.showMainView(panelMain);
             }
         });
-        nowyBiletCheckBox.addActionListener(new ActionListener() {
+
+        nowyBiletCheckBox.addItemListener(new ItemListener() {
             @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                textField1.setEnabled(!textField1.isEnabled());
+            public void itemStateChanged(ItemEvent e) {
+                if(e.getStateChange()==ItemEvent.SELECTED){
+                    wydrukujBiletButton.setEnabled(true);
+                    textField1.setEnabled(false);
+                    doładujBiletButton.setEnabled(false);
+                    PreparedStatement ps;
+                    ResultSet rs;
+                    String query = "SELECT AUTO_INCREMENT FROM information_schema.TABLES WHERE TABLE_SCHEMA = \"slavek_bd2\" AND TABLE_NAME = \"karnet\"";
+                    try {
+                        ps = MySQLConnection.getConnection().prepareStatement(query);
+                        rs = ps.executeQuery();
+
+                        if (rs.first()) {
+                            textField1.setText(rs.getString(1));
+                        }
+
+                    } catch (SQLException e1) {
+                        e1.printStackTrace();
+                    }
+                }
+                else{
+                    textField1.setText(null);
+                    textField1.setEnabled(true);
+                    doładujBiletButton.setEnabled(true);
+                    wydrukujBiletButton.setEnabled(false);
+                }
+            }
+        });
+        zablokujBiletButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                PreparedStatement ps;
+                ResultSet rs;
+                String ticketnumber= textField3.getText();
+                String query ="SELECT zablokowany FROM karnet WHERE id=?";
+
+                try {
+                    ps = MySQLConnection.getConnection().prepareStatement(query);
+                    ps.setString(1,ticketnumber);
+                    rs= ps.executeQuery();
+                    if(rs.first()) {
+                        int zab = rs.getInt("zablokowany");
+                        if (zab == 1) {
+                            JOptionPane.showMessageDialog(null, "Ten bilet jest już zablokowany");
+                        }
+                        else{
+                            String query1="UPDATE karnet SET zablokowany=1 WHERE id=?";
+                            PreparedStatement ps1 = MySQLConnection.getConnection().prepareStatement(query1);
+                            ps1.setString(1,ticketnumber);
+                            int rs1= ps1.executeUpdate();
+                        JOptionPane.showConfirmDialog(null, "Czy na pewno chcesz zablokować bilet");
+                        }
+                    }
+                    else{
+                        JOptionPane.showMessageDialog(null,"Nie ma takiego biletu");
+                    }
+                } catch (SQLException ex) {
+                    ex.printStackTrace();
+                }
+
+
             }
         });
     }
--- src/com/bdproj/MainWin.java
+++ src/com/bdproj/MainWin.java
@@ -22,37 +22,7 @@ public class MainWin implements MainView {
         btnLogin.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
-                PreparedStatement ps;
-                ResultSet rs;
-                String username= txtLogin.getText();
-                String password= String.valueOf(passwLogin.getPassword());
-                String query=   "SELECT 'K' as WHO FROM kierownik WHERE login like ? AND haslo like MD5(?)\n" +
-                                "union all\n" +
-                                "SELECT 'P' as WHO FROM pracownicy WHERE login like ? AND haslo like MD5(?)";
-            try {
-                ps = MySQLConnection.getConnection().prepareStatement(query);
-                ps.setString(1, username);
-                ps.setString(2, password);
-                ps.setString(3, username);
-                ps.setString(4, password);
-                rs = ps.executeQuery();
-
-                if (rs.first()) {
-                    String who = rs.getString("WHO");
-                    if (who.equals("K")) {
-                        showSupervisorView();
-                        return;
-                    } else if (who.equals("P")) {
-                        showEmployeeView();
-                        return;
-                    }
-                }
-
-                JOptionPane.showMessageDialog(null, "COS POSZLO NIE TAK ZIOMEK");
-
-                } catch(SQLException ex){
-                    Logger.getLogger(MainWin.class.getName()).log(Level.SEVERE,null, ex);
-                }
+            logincheckwho();
             }
         });
     }
@@ -64,19 +34,56 @@ public class MainWin implements MainView {
     public void showMainView(JPanel toHide) {
         frame.remove(toHide);
         frame.setContentPane(panelMain);
+        frame.setSize(500,200);
         panelMain.updateUI();
     }
 
     public void showSupervisorView() {
         frame.remove(panelMain);
         frame.setContentPane(supervisorWgt.getPanel());
+        frame.setSize(700,500);
         supervisorWgt.getPanel().updateUI();
     }
 
     public void showEmployeeView() {
         frame.remove(panelMain);
         frame.setContentPane(employeeWgt.getPanel());
+        frame.setSize(600,500);
         employeeWgt.getPanel().updateUI();
+    }
+
+    public void logincheckwho(){
+        PreparedStatement ps;
+        ResultSet rs;
+        String username= txtLogin.getText();
+        String password= String.valueOf(passwLogin.getPassword());
+        String query=   "SELECT 'K' as WHO FROM kierownik WHERE login like ? AND haslo like MD5(?)\n" +
+                "union all\n" +
+                "SELECT 'P' as WHO FROM pracownicy WHERE login like ? AND haslo like MD5(?)";
+        try {
+            ps = MySQLConnection.getConnection().prepareStatement(query);
+            ps.setString(1, username);
+            ps.setString(2, password);
+            ps.setString(3, username);
+            ps.setString(4, password);
+            rs = ps.executeQuery();
+
+            if (rs.first()) {
+                String who = rs.getString("WHO");
+                if (who.equals("K")) {
+                    showSupervisorView();
+                    return;
+                } else if (who.equals("P")) {
+                    showEmployeeView();
+                    return;
+                }
+            }
+
+            JOptionPane.showMessageDialog(null, "COS POSZLO NIE TAK ZIOMEK");
+
+        } catch(SQLException ex){
+            Logger.getLogger(MainWin.class.getName()).log(Level.SEVERE,null, ex);
+        }
     }
 
     public static void main(String[] args) {
