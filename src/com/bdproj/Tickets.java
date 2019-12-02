e2ce6f7756f07eb6ebe3dc5bdb4c4241cb5444a6
Dominik Kała <dominikkala@gmail.com>
2019-12-02 16:15:46 +0100

    Select najnowszego cennika

--- src/com/bdproj/Tickets.java
+++ src/com/bdproj/Tickets.java
@@ -8,6 +8,9 @@ public class Tickets {
     // TODO: Doladowywanie biletu.
     // TODO: Blokowanie biletow (ustawienie flagi).
 
+    // Pobieranie najnowszego cennika z bazy:
+    // select pc.id as 'poz_cennik_id', sc.nazwa, pc.cena from poz_cennik pc join slownik_cennik sc on pc.slownik_cennik_id = sc.id where pc.cennik_id = (select max(c.id) from cennik c);
+
     public Tickets(SystemUser user) {
         systemUser = user;
     }
