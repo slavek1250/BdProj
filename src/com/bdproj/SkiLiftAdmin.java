b7315b19c64cee8f7d377026ddd648cdd1ae1a45
kwojcik13 <klaudiawojcik10@op.pl>
2019-12-11 22:12:21 +0100

    Dodwanie nowego wyciągu

--- src/com/bdproj/SkiLiftAdmin.java
+++ src/com/bdproj/SkiLiftAdmin.java
@@ -1,14 +1,51 @@
 package com.bdproj;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class SkiLiftAdmin {
     private SystemUser systemUser;
 
-    // TODO: Dodawanie nowego wyciagu. #KLAUDIA#
+    // TODO: Dodawanie nowego wyciagu. #KLAUDIA# !!DONE!!
     // TODO: Pobieranie istniejacych wyciagow bedacych pod kierownikiem systemUser.getId(). #KLAUDIA#
     // TODO: Modyfikacja wyciagow bedacych pod jurysdykcja bierzacego kierownika. #SZYMON#
     // TODO: Usuwanie wyciagow (ustawienie flagi). #SZYMON#
 
-    public SkiLiftAdmin(SystemUser user) {
-        systemUser = user;
+    public SkiLiftAdmin(SystemUser user) { systemUser = user; }
+
+    public void addNewLift(String name, String height, String pointsCost, boolean state, int idSup){
+        PreparedStatement ps1;
+        PreparedStatement ps2;
+        PreparedStatement ps3;
+        ResultSet rs;
+        int idLift = 0;
+        String sql_1 = "INSERT INTO wyciag (nazwa, wysokosc) VALUES(?,?)";
+        String sql_2 = "INSERT INTO wyciag_dane (od, koszt_pkt, stan, wyciag_id, kierownik_id) VALUES(now(),?,?,?,?)";
+        String sql_3 = "SELECT id FROM wyciag WHERE nazwa=? AND wysokosc =?";
+        if(MySQLConnection.prepareConnection()) {
+            try {
+                ps1 = MySQLConnection.getConnection().prepareStatement(sql_1);
+                ps1.setString(1, name);
+                ps1.setString(2, height);
+                int rs1 = ps1.executeUpdate();
+                ps3 = MySQLConnection.getConnection().prepareStatement(sql_3);
+                ps3.setString(1,name);
+                ps3.setString(2,height);
+                rs = ps3.executeQuery();
+                if(rs.first()){
+                    idLift = rs.getInt("id");
+                }
+                ps2 = MySQLConnection.getConnection().prepareStatement(sql_2);
+                ps2.setString(1, pointsCost);
+                ps2.setBoolean(2, state);
+                ps2.setInt(3, idLift);
+                ps2.setInt(4,idSup);
+                int rs2 = ps2.executeUpdate();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
+
--- src/com/bdproj/SupervisorWgt.java
+++ src/com/bdproj/SupervisorWgt.java
@@ -1,5 +1,6 @@
 package com.bdproj;
 
+import java.lang.*;
 import javax.swing.*;
 import javax.swing.event.TableModelListener;
 import javax.swing.table.DefaultTableModel;
@@ -59,6 +60,7 @@ public class SupervisorWgt extends Supervisor {
     private String nameRegEx = "^[A-ZĄĆĘŁŃÓŚŹŻ]{1}[a-ząćęłńóśźż]{1,49}$";
     private String surnameRegEx = "^[A-ZĄĆĘŁŃÓŚŹŻ]{1}(([a-ząćęłńóśźż]+)(-[A-ZĄĆĘŁŃÓŚŹŻ]{1}[a-ząćęłńóśźż]+)?)$";
     private int surnameMaxLength = 50;
+    private String onlyNumbersRegEx = "\\d+";
     
     // TODO: Pracownicy: ladowanie pracownikow podleglych pod kierownika, walidacja danych wejsciowych. #Karol# !!DONE!!
     // TODO: Pracownicy: Mianowanie na kierownika, powinno automatycznie usuwać z listy pracowników pod kierownikiem ( w bazie ustaiwnie flagi jako pracownik zwolniony i kopia danych do kierownika ) #Karol#
@@ -86,7 +88,7 @@ public class SupervisorWgt extends Supervisor {
         btnDeleteEmpl.addActionListener(ActionEvent ->deleteEmployee());
         btnSaveSupervisor.addActionListener(ActionEvent -> updateSupervisorData());
         btnQuitJobSupervisor.addActionListener(ActionEvent -> quitJobSupervisor());
-
+        btnNewAddLift.addActionListener(ActionEvent -> addLift());
 
         loadPriceList();
         loadEmployees();
@@ -303,6 +305,27 @@ private void chooseUser(ActionEvent e){
         String surname=txtSurnameEditEmpl.getText();
         employeeAdmin.deleteEmployee();
 }
+
+    private void addLift(){
+        String name = txtNameNewLift.getText();
+        String height = txtHeightNewLift.getText();
+        String pointsCost = txtPointsCostNewLift.getText();
+        int idSup = systemUser.getId();
+        boolean state = checkStateNewLift.isSelected();
+        //skiLiftAdmin.addNewLift(name, height, pointsCost, state, idSup);
+        if(!height.matches(onlyNumbersRegEx) || !pointsCost.matches(onlyNumbersRegEx) ){
+            JOptionPane.showMessageDialog(null,"Niedozwolone dane wejściowe. Wysokość i koszt powinny być liczbą!");
+            return;
+        }
+        else{
+            int response= JOptionPane.showConfirmDialog(null, "Czy na pewno chcesz dodać wyciąg?","Confirm",JOptionPane.YES_NO_OPTION);
+            if(response==JOptionPane.YES_OPTION) {
+                skiLiftAdmin.addNewLift(name, height, pointsCost, state, idSup);
+                JOptionPane.showMessageDialog(null, "Dodano wyciąg");
+            }
+            else{return;}
+        }
+    }
 
 }
 
